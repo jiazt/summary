e8a0669eb307b774a74afccd4176d2d968358c60
zhentian.jia <zhentian.jia@picahealth.com>
2019-10-08 16:02:53 +0800

    initList_Sq

--- c/pdf.c
+++ c/pdf.c
@@ -40,3 +40,19 @@ void MergeList(List La,List Lb,List &Lc) {
 //线性表的存储结构
 #define List_Init_Size 100 //线性表存储空间 初始化分配量
 #define ListIncrement  10  //线性表存储空间 分配增量
+typedef struct {
+    ElemType * elem;  //存储空间基址
+    int length;   //当前长度
+    int listsize; //当前分配的存储容量
+}SqList;
+
+//构造一个空的线性表L
+Status InitList_Sq(SqList &L) {
+    L.elem = (ElemType *)malloc(List_Init_Size * sizeof(ElemType));
+    if(! L.elem) {
+        exit(OVERFLOW);
+    }
+    L.length = 0;
+    L.listsize = List_Init_Size;
+    return OK;
+}
